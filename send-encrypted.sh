99099c35dd1a636c8c0bdd501eb1c2589e8bb895
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-11-22 21:10:05 -0700

    Send notification when specified command finishes

--- send-encrypted.sh
+++ send-encrypted.sh
@@ -1,75 +1,96 @@
 #!/bin/sh
 
-usage() { echo "Usage: $0 -k <simplepush_key> -p <password> [-s <salt>] [-e <event>] [-t <title>] -m <message>" 1>&2; exit 0; }
+config_file="$HOME/.notify.conf"
 
-while getopts ":k:p:s:e:t:m:" o; do
-	case "${o}" in
-		k)
-			k=${OPTARG}
-			;;
-		p)
-			p=${OPTARG}
-			;;
-		s)
-			s=${OPTARG}
-			;;
-		e)
-			e=${OPTARG}
-			;;
-		t)
-			t=${OPTARG}
-			;;
-		m)
-			m=${OPTARG}
-			;;
-		*)
-			usage
-			;;
-	esac
-done
-shift $((OPTIND-1))
+create_config() {
+	echo "Config file not found. Creating a new one"
+	echo -n "Enter key: "
+	read key
+	echo -n "Enter salt: "
+	read salt
+	echo ""
+	echo -n "Enter password: "
+	read -s passwd
+	echo ""
 
-if [ -z "${k}" ] || [ -z "${p}" ] || [ -z "${m}" ]; then
-  usage
-	return 1
-fi
+	config="$key
+$salt
+$passwd"
+
+	res="$(openssl aes-256-cbc -a -salt -out $config_file <<< "$config")"
+}
+
+decrypt_config() {
+	params=()
+	res=$(openssl aes-256-cbc -d -a -in $config_file)
+	if [ "$?" -ne 0 ]; then
+		exit
+	fi
+
+	while read -r line; do
+		params+=("$line")
+	done <<< "$res"
+
+	key="${params[0]}"
+	salt="${params[1]}"
+	passwd="${params[2]}"
+}
 
 generate_key () {
-    # First argument is password
-	if [ -z "${s}" ]; then
-    	echo -n "${1}${salt}" | sha1sum | awk '{print toupper($1)}' | cut -c1-32
+	# First argument is password
+	if [ -z "${salt}" ]; then
+		echo -n "${1}${default_salt}" | sha1sum | awk '{print toupper($1)}' | cut -c1-32
 	else
-    	echo -n "${1}${s}" | sha1sum | awk '{print toupper($1)}' | cut -c1-32
+		echo -n "${1}${salt}" | sha1sum | awk '{print toupper($1)}' | cut -c1-32
 	fi
 }
 
 encrypt () {
-    # First argument is key
-    # Second argument is IV
-    # Third argument is data
+	# First argument is key
+	# Second argument is IV
+	# Third argument is data
 
-    echo -n "${3}" | openssl aes-128-cbc -base64 -K "${1}" -iv "${2}" | awk '{print}' ORS='' | tr '+' '-' | tr '/' '_'
+	echo -n "${3}" | openssl aes-128-cbc -base64 -K "${1}" -iv "${2}" | awk '{print}' ORS='' | tr '+' '-' | tr '/' '_'
 }
+
+run_command() {
+	command=""
+	for arg in "$@"; do
+		command+="$arg"
+		command+=" "
+	done
+
+	$SHELL -c "${command}"
+	sh_res="$?"
+}
+
+if [ -e $config_file ]; then
+	decrypt_config
+else
+	create_config
+fi
+
+run_command "${@}"
 
 iv=`openssl enc -aes-128-cbc -k dummy -P -md sha1 | grep iv | cut -d "=" -f 2`
 
-salt=1789F0B8C4A051E5
+default_salt=1789F0B8C4A051E5
 
-encryption_key=`generate_key "${p}"`
+encryption_key=`generate_key "${passwd}"`
 
-if [ -n "${t}" ]; then
-    title_encrypted=`encrypt "${encryption_key}" "${iv}" "${t}"`
-	  title="&title=${title_encrypted}"
+if [ ! -z $HOST ]; then
+	title_encrypted=`encrypt "${encryption_key}" "${iv}" "$HOST"`
 else
-	  title=""
+	title_encrypted=`encrypt "${encryption_key}" "${iv}" "$HOSTNAME"`
+fi
+title="&title=${title_encrypted}"
+
+if [ $sh_res -eq 0 ]; then
+	event="&event=success"
+else
+	event="&event=error"
 fi
 
-if [ -n "${e}" ]; then
-	  event="&event=${e}"
-else
-	  event=""
-fi
+message=`encrypt "${encryption_key}" "${iv}" "${1}"`
 
-message=`encrypt "${encryption_key}" "${iv}" "${m}"`
-
-curl --data "key=${k}${title}&msg=${message}${event}&encrypted=true&iv=$iv" "https://api.simplepush.io/send" > /dev/null 2>&1
+curl --http1.1 --data "key=${key}${title}&msg=${message}${event}&encrypted=true&iv=$iv" "https://api.simplepush.io/send" > /dev/null 2>&1
